508e226e048d486f4ab935addd9c08d0c7ecd5e3
Charlene <charlene.martello123@gmail.com>
2024-10-22 15:47:22 -0300

    ajuste

--- mod19-streamlitbankmarketing.py
+++ mod19-streamlitbankmarketing.py
@@ -90,214 +90,221 @@ def main():
             bank = bank_raw.copy()
         else:
             bank=None
+    else:
+        # Exibe a mensagem solicitada enquanto o arquivo não é carregado
+        st.write("Por favor, para ter acesso às funcionalidades dessa página, carregue o arquivo.")
 
+    # Executa o código abaixo apenas se o arquivo foi carregado
+    if bank_raw is not None:
+    
     #Após verificar qual o arquivo carregado e tendo convertido-o para o
     #formato csv para exibição e posteriormente ter feito sua cópia, vamos
     #agora exibir esse dataframe bruto:
-    st.subheader('Antes dos filtros:')
-    st.write(bank_raw.head())
+        st.subheader('Antes dos filtros:')
+        st.write(bank_raw.head())
     
-    #INÍCIO DA SELEÇÃO DE FILTROS:
-    with st.sidebar.form(key='my_form'):
+        #INÍCIO DA SELEÇÃO DE FILTROS:
+        with st.sidebar.form(key='my_form'):
           
-        # IDADES
-        max_age = int(bank.age.max())
-        min_age = int(bank.age.min())
-        idades = st.sidebar.slider(label='Idade',
+            # IDADES
+            max_age = int(bank.age.max())
+            min_age = int(bank.age.min())
+            idades = st.sidebar.slider(label='Idade',
                             min_value=min_age,
                             max_value=max_age,
                             value=(min_age, max_age),
                             step=1)
        
-        # PROFISSÕES
-        jobs_list = bank.job.unique().tolist()
-        jobs_list.append('all')
-        jobs_select = st.multiselect("Profissão", jobs_list, ['all'])
+            # PROFISSÕES
+            jobs_list = bank.job.unique().tolist()
+            jobs_list.append('all')
+            jobs_select = st.multiselect("Profissão", jobs_list, ['all'])
                              
-        # ESTADO CIVIL
-        marital_list = bank.marital.unique().tolist()
-        marital_list.append('all')
-        marital_selected = st.multiselect("Estado civil", marital_list, ['all'])
+            # ESTADO CIVIL
+            marital_list = bank.marital.unique().tolist()
+            marital_list.append('all')
+            marital_selected = st.multiselect("Estado civil", marital_list, ['all'])
 
-        # DEFAULT?
-        default_list = bank.default.unique().tolist()
-        default_list.append('all')
-        default_selected = st.multiselect("Default", default_list, ['all'])
+            # DEFAULT?
+            default_list = bank.default.unique().tolist()
+            default_list.append('all')
+            default_selected = st.multiselect("Default", default_list, ['all'])
 
-        # TEM FINANCIAMENTO IMOBILIÁRIO?
-        housing_list = bank.housing.unique().tolist()
-        housing_list.append('all')
-        housing_selected = st.multiselect("Tem financiamento imob?", housing_list, ['all'])
+            # TEM FINANCIAMENTO IMOBILIÁRIO?
+            housing_list = bank.housing.unique().tolist()
+            housing_list.append('all')
+            housing_selected = st.multiselect("Tem financiamento imob?", housing_list, ['all'])
 
-        # TEM EMPRÉSTIMO?
-        loan_list = bank.loan.unique().tolist()
-        loan_list.append('all')
-        loan_selected = st.multiselect("Tem empréstimo", loan_list, ['all'])
+            # TEM EMPRÉSTIMO?
+            loan_list = bank.loan.unique().tolist()
+            loan_list.append('all')
+            loan_selected = st.multiselect("Tem empréstimo", loan_list, ['all'])
 
-        # MEIO DE CONTATO?
-        contact_list = bank.contact.unique().tolist()
-        contact_list.append('all')
-        contact_selected = st.multiselect("Meio de contato?", contact_list, ['all'])
+            # MEIO DE CONTATO?
+            contact_list = bank.contact.unique().tolist()
+            contact_list.append('all')
+            contact_selected = st.multiselect("Meio de contato?", contact_list, ['all'])
 
-        # MÊS DE CONTATO?
-        month_list = bank.month.unique().tolist()
-        month_list.append('all')
-        month_selected = st.multiselect("Mês de contato?", month_list, ['all'])
+            # MÊS DE CONTATO?
+            month_list = bank.month.unique().tolist()
+            month_list.append('all')
+            month_selected = st.multiselect("Mês de contato?", month_list, ['all'])
 
-        # DIA DA SEMANA  
-        day_of_week_list = bank.day_of_week.unique().tolist()
-        day_of_week_list.append('all')
-        day_of_week_selected = st.multiselect("Tem financiamento imob?", day_of_week_list, ['all'])
+            # DIA DA SEMANA  
+            day_of_week_list = bank.day_of_week.unique().tolist()
+            day_of_week_list.append('all')
+            day_of_week_selected = st.multiselect("Tem financiamento imob?", day_of_week_list, ['all'])
 
-        bank = (bank.query("age >= @idades[0] and age <= @idades[1]")
-                .pipe(multiselect_filter, 'job', jobs_select)
-                .pipe(multiselect_filter, 'marital', marital_selected)
-                .pipe(multiselect_filter, 'default', default_selected)
-                .pipe(multiselect_filter, 'housing', housing_selected)
-                .pipe(multiselect_filter, 'loan', loan_selected)
-                .pipe(multiselect_filter, 'contact', contact_selected)
-                .pipe(multiselect_filter, 'month', month_selected)
-                .pipe(multiselect_filter, 'day_of_week', day_of_week_selected)
-        )
+            #APLICAÇÃO DOS FILTROS
+            bank = (bank.query("age >= @idades[0] and age <= @idades[1]")
+                    .pipe(multiselect_filter, 'job', jobs_select)
+                    .pipe(multiselect_filter, 'marital', marital_selected)
+                    .pipe(multiselect_filter, 'default', default_selected)
+                    .pipe(multiselect_filter, 'housing', housing_selected)
+                    .pipe(multiselect_filter, 'loan', loan_selected)
+                    .pipe(multiselect_filter, 'contact', contact_selected)
+                    .pipe(multiselect_filter, 'month', month_selected)
+                    .pipe(multiselect_filter, 'day_of_week', day_of_week_selected)
+            )
 
-        #SELECIONA O TIPO DE GRÁFICO:
-        graph_type= st.radio('Tipo de gráfico:', ('Barras', 'Pizza'))
+            #SELECIONA O TIPO DE GRÁFICO:
+            graph_type= st.radio('Tipo de gráfico:', ('Barras', 'Pizza'))
 
-        #BOTÃO DE APLICAÇÃO DOS FILTROS ACIMA:
-        submit_button = st.form_submit_button(label='Aplicar')
-    
-    #EXIBIÇÃO DO DF FILTRADO:
-    st.write('Depois dos filtros:')
-    st.write(bank.head())
-
-    #BOTÃO PARA DOWNLOAD DA PLANILHA (DF) FORMATO EXCEL:
-    df_xlsx = df_to_excel(bank)
-    st.download_button(label= 'DOWNLOAD TABELA FILTRADA EM EXCEL',
-                       data=df_xlsx,
-                       file_name='bank_filtred.xlsx')
-    st.markdown("---")
-
-#TABELAS PROPORÇÃO DE ACEITE
-#Pré exibição dos gráficos, vamos exibir uma tabela com o mesmo objetivo,
-# mostrar a proporção de aceite para de emprestimo para os dados brutos e 
-# filtrados: 
-    # Contagem dos valores para os dados brutos
-    bank_raw_counts = bank_raw['y'].value_counts(normalize=True).reset_index()
-    bank_raw_counts.columns = ['Resposta', 'Porcentagem']
-    bank_raw_counts['Porcentagem'] *= 100  # Convertendo para porcentagem
-
-    # Contagem dos valores para os dados filtrados
-    bank_filtered_counts = bank['y'].value_counts(normalize=True).reset_index()
-    bank_filtered_counts.columns = ['Resposta', 'Porcentagem']
-    bank_filtered_counts['Porcentagem'] *= 100  # Convertendo para porcentagem
-
-    # Mostrar as tabelas de proporção de aceite
-    st.subheader("Proporção de aceite:")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("Dados Brutos")
-        st.write(bank_raw_counts)
-
-    with col2:
-        st.subheader("Dados Filtrados")
-        st.write(bank_filtered_counts)
-
-    # Botões de download das tabelas
-    if bank_raw_counts is not None:
-        csv_raw_summary = df_to_csv(bank_raw_counts)
-        st.download_button(
-            label='DOWNLOAD PROPORÇÃO DE ACEITE DADOS BRUTOS CSV',
-            data=csv_raw_summary,
-            file_name='bank_raw_summary.csv',
-            mime='text/csv'
-        )
-
-    if bank_filtered_counts is not None:
-        csv_filtered_summary = df_to_csv(bank_filtered_counts)
-        st.download_button(
-            label='DOWNLOAD PROPORÇÃO DE ACEITE DADOS FILTRADOS CSV',
-            data=csv_filtered_summary,
-            file_name='bank_filtered_summary.csv',
-            mime='text/csv'
-        )
-
-    st.markdown("---")    
-
-# GRÁFICOS
-#Agora, de acordo com a seleção do tipo de gráfico feita anteriormente pelo
-#usuário, vamos plotar nossos gráficos. 
-
-    st.subheader("Gráficos proporção de aceite:")
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5)) #TAMANHO
-
-    if graph_type == 'Barras':
-    # Dataset bruto:
-        bank_raw_target_perc = bank_raw['y'].value_counts(normalize=True).to_frame(name='percentage') * 100
-        bank_raw_target_perc = bank_raw_target_perc.reset_index().rename(columns={'index': 'y'})
-        sns.barplot(x='y', y='percentage', data=bank_raw_target_perc, ax=ax[0])
-        ax[0].bar_label(ax[0].containers[0])
-        ax[0].set_title('Dados brutos', fontweight="bold")
-
-    # Dataset filtrado
-        bank_target_perc = bank['y'].value_counts(normalize=True).to_frame(name='percentage') * 100
-        bank_target_perc = bank_target_perc.reset_index().rename(columns={'index': 'y'})
-        sns.barplot(x='y', y='percentage', data=bank_target_perc, ax=ax[1])
-        ax[1].bar_label(ax[1].containers[0])
-        ax[1].set_title('Dados filtrados', fontweight="bold")
-    else:
-    # Gráfico de pizza para dados brutos
-        ax[0].pie(bank_raw['y'].value_counts(), labels=bank_raw['y'].value_counts().index, autopct='%1.1f%%', startangle=140)
-        ax[0].set_title('Dados brutos')
-        ax[0].axis('equal')
-
-    # Gráfico de pizza para dados filtrados
-        ax[1].pie(bank['y'].value_counts(), labels=bank['y'].value_counts().index, autopct='%1.1f%%', startangle=140)
-        ax[1].set_title('Dados filtrados')
-        ax[1].axis('equal')
-
-# Mostrar os gráficos
-    st.pyplot(fig)
-
-# BOTÕES DE DOWNLOAD DOS GRÁFICOS
-    if 'graph_type' in locals():
-        if graph_type == 'Barras':
-            png_data = BytesIO()
-            fig.savefig(png_data, format='png')
-            st.download_button(
-                label='DOWNLOAD GRÁFICOS DE BARRAS',
-                data=png_data,
-                file_name='grafico_de_barras.png',
-                mime='image/png'
-        )
-        else:
-            png_data = BytesIO()
-            fig.savefig(png_data, format='png')
-            st.download_button(
-                label='DOWNLOAD GRÁFICOS DE PIZZA',
-                data=png_data,
-                file_name='grafico_de_pizza.png',
-                mime='image/png'
-        )
-    st.markdown("---") 
-
-    # Botões de download de arquivo
-    if data_file_1 is not None:
-        csv_data = df_to_csv(bank_raw)
-        st.write('Download do Data Frame utilizado em formato CSV:')
-        st.download_button(
-            label='DOWNLOAD DATA AS CSV',
-            data=csv_data,
-            file_name='df_csv.csv',
-            mime='text/csv'
-        )
+            #BOTÃO DE APLICAÇÃO DOS FILTROS ACIMA:
+            submit_button = st.form_submit_button(label='Aplicar')
         
-        excel_data = df_to_excel(bank_raw)
-        st.write('Download do Data Frame utilizado em formato xlsx:')
-        st.download_button(
-            label='Download Data as EXCEL',
-            data=excel_data,
-            file_name='df_excel.xlsx'
-        )
+        #EXIBIÇÃO DO DF FILTRADO:
+        st.write('Depois dos filtros:')
+        st.write(bank.head())
+
+        #BOTÃO PARA DOWNLOAD DA PLANILHA (DF) FORMATO EXCEL:
+        df_xlsx = df_to_excel(bank)
+        st.download_button(label= 'DOWNLOAD TABELA FILTRADA EM EXCEL',
+                        data=df_xlsx,
+                        file_name='bank_filtred.xlsx')
+        st.markdown("---")
+
+    #TABELAS PROPORÇÃO DE ACEITE
+    #Pré exibição dos gráficos, vamos exibir uma tabela com o mesmo objetivo,
+    # mostrar a proporção de aceite para de emprestimo para os dados brutos e 
+    # filtrados: 
+        # Contagem dos valores para os dados brutos
+        bank_raw_counts = bank_raw['y'].value_counts(normalize=True).reset_index()
+        bank_raw_counts.columns = ['Resposta', 'Porcentagem']
+        bank_raw_counts['Porcentagem'] *= 100  # Convertendo para porcentagem
+
+        # Contagem dos valores para os dados filtrados
+        bank_filtered_counts = bank['y'].value_counts(normalize=True).reset_index()
+        bank_filtered_counts.columns = ['Resposta', 'Porcentagem']
+        bank_filtered_counts['Porcentagem'] *= 100  # Convertendo para porcentagem
+
+        # Mostrar as tabelas de proporção de aceite
+        st.subheader("Proporção de aceite:")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader("Dados Brutos")
+            st.write(bank_raw_counts)
+
+        with col2:
+            st.subheader("Dados Filtrados")
+            st.write(bank_filtered_counts)
+
+        # Botões de download das tabelas
+        if bank_raw_counts is not None:
+            csv_raw_summary = df_to_csv(bank_raw_counts)
+            st.download_button(
+                label='DOWNLOAD PROPORÇÃO DE ACEITE DADOS BRUTOS CSV',
+                data=csv_raw_summary,
+                file_name='bank_raw_summary.csv',
+                mime='text/csv'
+            )
+
+        if bank_filtered_counts is not None:
+            csv_filtered_summary = df_to_csv(bank_filtered_counts)
+            st.download_button(
+                label='DOWNLOAD PROPORÇÃO DE ACEITE DADOS FILTRADOS CSV',
+                data=csv_filtered_summary,
+                file_name='bank_filtered_summary.csv',
+                mime='text/csv'
+            )
+
+        st.markdown("---")    
+
+    # GRÁFICOS
+    #Agora, de acordo com a seleção do tipo de gráfico feita anteriormente pelo
+    #usuário, vamos plotar nossos gráficos. 
+
+        st.subheader("Gráficos proporção de aceite:")
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5)) #TAMANHO
+
+        if graph_type == 'Barras':
+        # Dataset bruto:
+            bank_raw_target_perc = bank_raw['y'].value_counts(normalize=True).to_frame(name='percentage') * 100
+            bank_raw_target_perc = bank_raw_target_perc.reset_index().rename(columns={'index': 'y'})
+            sns.barplot(x='y', y='percentage', data=bank_raw_target_perc, ax=ax[0])
+            ax[0].bar_label(ax[0].containers[0])
+            ax[0].set_title('Dados brutos', fontweight="bold")
+
+        # Dataset filtrado
+            bank_target_perc = bank['y'].value_counts(normalize=True).to_frame(name='percentage') * 100
+            bank_target_perc = bank_target_perc.reset_index().rename(columns={'index': 'y'})
+            sns.barplot(x='y', y='percentage', data=bank_target_perc, ax=ax[1])
+            ax[1].bar_label(ax[1].containers[0])
+            ax[1].set_title('Dados filtrados', fontweight="bold")
+        else:
+        # Gráfico de pizza para dados brutos
+            ax[0].pie(bank_raw['y'].value_counts(), labels=bank_raw['y'].value_counts().index, autopct='%1.1f%%', startangle=140)
+            ax[0].set_title('Dados brutos')
+            ax[0].axis('equal')
+
+        # Gráfico de pizza para dados filtrados
+            ax[1].pie(bank['y'].value_counts(), labels=bank['y'].value_counts().index, autopct='%1.1f%%', startangle=140)
+            ax[1].set_title('Dados filtrados')
+            ax[1].axis('equal')
+
+    # Mostrar os gráficos
+        st.pyplot(fig)
+
+    # BOTÕES DE DOWNLOAD DOS GRÁFICOS
+        if 'graph_type' in locals():
+            if graph_type == 'Barras':
+                png_data = BytesIO()
+                fig.savefig(png_data, format='png')
+                st.download_button(
+                    label='DOWNLOAD GRÁFICOS DE BARRAS',
+                    data=png_data,
+                    file_name='grafico_de_barras.png',
+                    mime='image/png'
+            )
+            else:
+                png_data = BytesIO()
+                fig.savefig(png_data, format='png')
+                st.download_button(
+                    label='DOWNLOAD GRÁFICOS DE PIZZA',
+                    data=png_data,
+                    file_name='grafico_de_pizza.png',
+                    mime='image/png'
+            )
+        st.markdown("---") 
+
+        # Botões de download de arquivo
+        if data_file_1 is not None:
+            csv_data = df_to_csv(bank_raw)
+            st.write('Download do Data Frame utilizado em formato CSV:')
+            st.download_button(
+                label='DOWNLOAD DATA AS CSV',
+                data=csv_data,
+                file_name='df_csv.csv',
+                mime='text/csv'
+            )
+            
+            excel_data = df_to_excel(bank_raw)
+            st.write('Download do Data Frame utilizado em formato xlsx:')
+            st.download_button(
+                label='Download Data as EXCEL',
+                data=excel_data,
+                file_name='df_excel.xlsx'
+            )
 
 if __name__ == "__main__":
     main()
